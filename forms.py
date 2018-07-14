4092cb51ce706f936c130dfa7355b5d52493509a
alphardex <2582347430@qq.com>
2018-07-14 20:52:13 +0800

    add video platforms

--- forms.py
+++ forms.py
@@ -4,12 +4,12 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
-def make_choice():
+def make_choice(api_name):
     api = requests.get('https://iodefog.github.io/text/viplist.json').json()
-    return [(a['url'], a['name']) for a in api['list']]
+    return [(a['url'], a['name']) for a in api[api_name]]
 
 
 class VipListForm(FlaskForm):
-    parser = SelectField('解析线路', validators=[DataRequired()], choices=make_choice(), coerce=str)
+    parser = SelectField('解析线路', validators=[DataRequired()], choices=make_choice('list'), coerce=str)
     url = StringField('视频地址', validators=[DataRequired()])
     submit = SubmitField('解析')
--- ivideo.py
+++ ivideo.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
-from forms import VipListForm
+from forms import VipListForm, make_choice
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,6 +9,7 @@ app.config['SECRET_KEY'] = 'gds324g3434'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = VipListForm()
+    platforms = make_choice('platformlist')
     if form.validate_on_submit():
         return redirect(f'{form.parser.data}{form.url.data}')
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, platforms=platforms)
